35d837d3905c5c85839d40e8511eba1495d65b1d
lookfiresu123 <lookfiresu123@gmail.com>
2016-07-28 22:42:22 +0800

    update memory_pool

--- memory_pool/memory_pool.h
+++ memory_pool/memory_pool.h
@@ -50,9 +50,13 @@ public:
                 data_pointer_ body = new_block + sizeof(slot_pointer_);
                 uintptr_t result = reinterpret_cast<uintptr_t>(body);
                 size_t bodyPadding = (alignof(slot_type_) - result) % alignof(slot_type_);
+
+                // 更新当前对象槽为新的内存区块的第一个对象槽
                 currentSlot_ = reinterpret_cast<slot_pointer_>(body + bodyPadding);
+                // 更新最后一个对象槽为新的内存区块的最后一个对象槽
                 lastSlot_ = reinterpret_cast<slot_pointer_>(new_block + BlockSize - sizeof(slot_type_) + 1);
             }
+            // 返回第一个空闲（当前）的对象槽，并将更新对象槽
             return reinterpret_cast<pointer>(currentSlot_++);
         }
     }
@@ -60,8 +64,8 @@ public:
     // 销毁指针p所指向的内存块
     void deallocate(pointer p, size_t n = 1) {
         if (p != nullptr) {
-            reinterpret_cast<slot_pointer_>(p)->next = freeSlot_;
-            freeSlot_ = reinterpret_cast<slot_pointer_>(p);
+            reinterpret_cast<slot_pointer_>(p)->next = freeSlot_;   // 将p所指对象槽加入空闲对象槽
+            freeSlot_ = reinterpret_cast<slot_pointer_>(p);         // 更新指向第一个空闲对象槽的指针
         }
     }
 
@@ -78,18 +82,18 @@ public:
 private:
     // 内存池中用于存储对象的槽，要么存储对象，要么存储指针
     union Slot_ {
-        T element;
-        Slot_ *next;
+        T element;          // 对象槽中的对象
+        Slot_ *next;        // 指向下一个对象槽的指针
     };
 
     typedef char *  data_pointer_;  // 数据指针
-    typedef Slot_   slot_type_;     // 对象槽
+    typedef Slot_   slot_type_;     // 对象槽类型
     typedef Slot_ * slot_pointer_;  // 指向对象槽的指针
 
-    slot_pointer_ currentBlock_;    // 指向当前内存区块（一个内存区块有多个对象槽）
-    slot_pointer_ currentSlot_;     // 指向当前内存区块中的当前对象槽
-    slot_pointer_ lastSlot_;        // 指向当前内存区块中的最后一个对象槽
-    slot_pointer_ freeSlot_;        // 指向当前内存区块中的空闲对象槽
+    slot_pointer_ currentBlock_;    // 指向当前内存区块（一个内存区块有多个对象槽）的指针
+    slot_pointer_ currentSlot_;     // 指向当前内存区块中的当前对象槽的指针
+    slot_pointer_ lastSlot_;        // 指向当前内存区块中的最后一个对象槽的指针
+    slot_pointer_ freeSlot_;        // 指向当前内存区块中的第一个空闲对象槽的指针
 
     // 检查定义的内存池大小是否过小
     static_assert(BlockSize >= 2 * sizeof(slot_type_), "BlockSize is too small.");
